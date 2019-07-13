16c73b3fae9b1c81c77aa7da33538728fe3aad41
AJie <kevinsalvatore@qq.com>
2019-07-13 13:09:02 +0800

    Add socket.IO to this project!

--- app.js
+++ app.js
@@ -4,9 +4,13 @@ const bodyParser = require("koa-bodyparser");
 const passport = require("koa-passport");
 const { serverPort } = require("./config/index");
 const cors = require("koa-cors");
+const IO = require("koa-socket-2");
 
 const app = new Koa();
 const router = new Router();
+const io = new IO();
+
+io.attach(app);
 
 app.use(cors());
 
@@ -20,6 +24,11 @@ const user = require("./routes/API/user/index");
 router.use("/API/user", user);
 
 app.use(router.routes()).use(router.allowedMethods());
+
+app.listen = function() {
+  app.server.listen.apply(app.server, arguments);
+  return app.server;
+};
 
 app.listen(serverPort, () => {
   console.log(`Server is running at ${serverPort} port!`);
